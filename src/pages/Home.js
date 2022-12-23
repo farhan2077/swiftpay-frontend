e10ad694620624d81452f73986748d2062df34c2
Farhan Bin Amin <farhanbinamin@gmail.com>
2022-12-23 12:28:40 +0600

    new: added antd and removed unnecessary packages + initial config

---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Button } from "antd";
+
+export default function Home() {
+  return (
+    <div>
+      Home
+      <br />
+      <Button type="primary">Primary Button</Button>
+    </div>
+  );
+}
