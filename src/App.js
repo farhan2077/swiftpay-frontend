1746c59b19bea520ea82fd8fe3e6fc0e16c6f58f
Farhan Bin Amin <farhanbinamin@gmail.com>
2022-12-23 20:59:34 +0600

    new: added react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
+import { Routes, Route } from "react-router-dom";
+
 import Home from "./pages/Home";
+import Transaction from "./pages/Transaction";
 
 function App() {
   return (
     <div>
-      <Home />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/transactions/:vehicleId" element={<Transaction />} />
+      </Routes>
     </div>
   );
 }
