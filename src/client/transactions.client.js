b01c74b2e22e1a971c417d43285865f5dc5bd650
Farhan Bin Amin <farhanbinamin@gmail.com>
2022-12-31 00:06:36 +0600

    fix: can't read environment variable

--- src/client/transactions.client.js
+++ src/client/transactions.client.js
@@ -1,7 +1,4 @@
-const baseUrl =
-  process.env.NOVE_ENV !== "production"
-    ? process.env.REACT_APP_DEV_API_URL
-    : process.env.REACT_APP_PROD_API_URL;
+const baseUrl = process.env.REACT_APP_PROD_API_URL;
 
 export const getTransaction = async (vehicleId) => {
   return fetch(`${baseUrl}/transactions/${vehicleId}`, {
